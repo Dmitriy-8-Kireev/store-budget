aeb8166fedd135d1d9c0709632cb795da9233264
Dmitriy Kireev <chief_programmer@mail.ru>
2018-10-02 18:54:45 +0300

    add Market in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Market from '../Market';
-import Farm from '../Farm';
-import Budget from '../Budget';
 
 export class App extends Component {
   render() {
-    return <div className="app" />;
+    return (
+      <div className="app">
+        <Market />
+      </div>
+    );
   }
 }
-
 export default App;
