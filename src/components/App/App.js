3627590a9dde10c1cb4fcc3d2dafb920813cace0
Dmitriy Kireev <chief_programmer@mail.ru>
2018-10-02 19:14:39 +0300

    add Farm in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Market from '../Market';
+import Farm from '../Farm';
 
 export class App extends Component {
   render() {
     return (
       <div className="app">
         <Market />
+        <Farm />
       </div>
     );
   }
